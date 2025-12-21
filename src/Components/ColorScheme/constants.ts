b413dcb796c2f56f163f5271dc2407eb47c9c288
Km.Van <kmvan.com@gmail.com>
2025-12-21 21:39:57 +0800

    remove color scheme

--- src/Components/ColorScheme/constants.ts
+++
@@ -1,5 +0,0 @@
-class Main {
-  readonly id = 'colorScheme';
-  readonly storageId = 'schemeId';
-}
-export const ColorSchemeConstants = new Main();
--- src/Components/ColorScheme/stores/color-schemes.ts
+++
@@ -1,7 +0,0 @@
-import type { ColorSchemesProps } from '../typings';
-import { colorSchemeDark } from './color-scheme-dark';
-import { colorSchemeDefault } from './color-scheme-default';
-export const colorSchemes: ColorSchemesProps = {
-  default: colorSchemeDefault,
-  dark: colorSchemeDark,
-};
