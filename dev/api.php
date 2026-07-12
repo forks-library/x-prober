0f0c6c4d0d5749fad0f4ca5019832e32e07f76fb
Km.Van <kmvan.com@gmail.com>
2026-07-12 22:13:33 +0800

    update dev file

--- dev/api.php
+++ dev/api.php
@@ -1,7 +1,9 @@
 <?php
 namespace InnStudio\Prober\Components\PreDefine;
 $version = phpversion();
-version_compare($version, '5.4.0','<') && exit("PHP 5.4+ is required. Currently installed version is: {$version}");
+if (version_compare($version, '5.4.0', '<')) {
+    exit("需要 PHP 5.4 或更高版本。当前安装的版本是：{$version} | PHP 5.4+ is required. Currently installed version is: {$version}");
+}
 \define('XPROBER_TIMER', \microtime(true));
 \define('XPROBER_IS_DEV', true);
 \define('XPROBER_DIR', __DIR__);
